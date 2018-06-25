2718c552dfa956b1cefb6d45fcd9ebfd8aa1a8bc
Rachel Blades (Vega Consulting LLC) <v-rablad@microsoft.com>
2018-06-25 14:19:01 -0700

    adding sample data

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 var port = 3000;
-var requestHandler = function(request, response){
-    console.log('New request to: ' + request.url);
-    response.end('Hello, World!');
-}
-var server = http.createServer(requestHandler);
-server.listen(port, function(){
-    console.log('listening on port ' + port);
-   });
+
+var quotes = [
+    {
+        id: 1,
+        quote: "The best is yet to come",
+        author: "Unknown",
+        year: 2000
+    },
+    {
+        id: 2,
+        quote: "This is a quote",
+        author: "First Last",
+        year: 1930
+    },
+    {
+        id: 3,
+        quote: "This is another quote",
+        author: "First2 Last2",
+        year: 1910
+    }
+    ];
+
+app.get(`/`, function(request, response){
+    response.send("Get requested at '/'");
+});
+
+app.listen(port, function(){
+    console.log('Express app listening on port ' + port);
+});
