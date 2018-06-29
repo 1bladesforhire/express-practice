23fca70bab7b9fef431a9ee8c11b9af89d60453a
Rachel Blades (Vega Consulting LLC) <v-rablad@microsoft.com>
2018-06-29 16:33:36 -0700

    adding route results processing

--- server.js
+++ server.js
@@ -5,31 +5,17 @@ var sqlite3 = require('sqlite3');
 
 //create the express app
 var app = express();
+
+//assign a local port to listen on
 var port = 3000;
+
 //have express use body-parser for request body
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//sample data for playtime
-var quotes = [
-    {
-        id: 1,
-        quote: "The best is yet to come",
-        author: "Unknown",
-        year: 2000
-    },
-    {
-        id: 2,
-        quote: "This is a quote",
-        author: "First Last",
-        year: 1930
-    },
-    {
-        id: 3,
-        quote: "This is another quote",
-        author: "First2 Last2",
-        year: 1910
-    }
-    ];
+//add a database for persistent data storage
+var db = new sqlite3.Database('quotes.db');
+
+    
 
 //set the default response
 app.get(`/`, function(request, response){
@@ -38,26 +24,59 @@ app.get(`/`, function(request, response){
 
 //set up GET routes for quotes
 app.get('/quotes', function(req, res){
-    //check for year query parameters
     if(req.query.year){
-        res.send("Return a list of quotes from the year: " + req.query.year);
-      }
-      else{
-          res.json(quotes);
-      }
+
+        db.all('SELECT * FROM Quotes WHERE year=?', req.query.year,function(err, rows){
+            
+            if(err){
+                res.send(err.message);
+            }
+            else{
+                console.log("Return a list of quotes from the year: " + req.query.year);
+                res.json(rows);
+            }
+        });
+    }
+    else{
+        db.all('SELECT * FROM Quotes', function processRows(err, rows){
+            if(err){
+                res.send(err.message);
+            }
+            else{
+                for( var i = 0; i < rows.length; i++){
+                    console.log(rows[i].quote);
+                }
+                res.json(rows);
+            }
+        });
+    }
 });
 
 //GET function for id specific requests
 app.get('/quotes/:id', function(req, res){
     console.log("return quote with the ID: " + req.params.id);
-    res.send("Return quote with the ID: " + req.params.id);
+    db.get('SELECT * FROM quotes WHERE rowid = ?', [req.params.id], function(err, row){
+        if(err){
+            console.log(err.message);
+        }
+        else{
+            res.json(row);
+        }
+    });
 });
 
 //POST to create new quote
 app.post('/quotes', function(req, res){
     console.log("Insert a new quote: " + req.body.quote);
-    res.json(req.body);
-  });
+    db.run('INSERT INTO quotes VALUES (?, ?, ?)', [req.body.quote, req.body.author, req.body.year], function(err){
+        if(err){
+            console.log(err.message);
+        }
+        else{
+            res.send('Inserted quote with id: ' + this.lastID);
+        }
+    });
+});
 
 app.listen(port, function(){
     console.log('Express app listening on port ' + port);
